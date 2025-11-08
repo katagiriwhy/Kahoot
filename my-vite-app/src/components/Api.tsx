acdad2962d24fcf49c21c7c1883c0c477167458d
Аюпов Дима <dimaaupov2005@gmail.com>
2025-11-08 20:38:30 +0300

    added queries

--- my-vite-app/src/components/Api.tsx
+++ my-vite-app/src/components/Api.tsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 export const LOGIN_URL = '/auth';
 export const NEW_QUIZ_URL = '/new_quiz';
 export const REGISTER_URL = '/register';
+export const JOIN_URL = '/join';
+export const CREATE_URL = '/create';
 
 const api = axios.create({
   baseURL: 'http://localhost:8080',
