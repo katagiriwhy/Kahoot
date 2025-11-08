26065a90690e2f58cbf6deb788f33cada73e74c3
Аюпов Дима <dimaaupov2005@gmail.com>
2025-11-08 15:00:29 +0300

    router in app

--- my-vite-app/src/App.tsx
+++ my-vite-app/src/App.tsx
@@ -1,11 +1,20 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/Register'
+import Login from './components/Login'
 
 function App() {
 
   return (
     <>
-      <Register/>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* Временно */}
+          <Route path="/" element={<Register />} /> 
+        </Routes>
+      </Router>
     </>
   )
 }
